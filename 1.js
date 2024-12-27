f567f334c3cea084975788c17d9e97d06cc1b9b7
chan8632 <duscks8632@gmail.com>
2024-12-27 16:22:39 +0900

    입력값 초기상태

--- 1.js
+++ 1.js
@@ -2,15 +2,6 @@
 const fs = require("fs");
 const input = fs.readFileSync("./tc.txt").toString();
 
-const word = input.split("");
-let result = '';
-for (let i = 0; i < word.length; i++) {
-  if (i === word.length - 1) {
-    continue;
-  }
-  result += word[i] + " " + word[i + 1] + "\n";
-}
-console.log(result);
 
 
 

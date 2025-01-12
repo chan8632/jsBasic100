aa7e6d21d75be531bffcf7150afaea4da5d2272b
chan8632 <duscks8632@gmail.com>
2025-01-12 18:06:23 +0900

    1위부터 3위까지 사탕 주는 문제 범위값이 이상함2

--- 1.js
+++ 1.js
@@ -4,17 +4,21 @@ const input = fs.readFileSync("./tc.txt").toString();
 
 const scoreList = input.split(" ").map(Number);
 const sortScoreList = scoreList.sort((a, b) => a - b);
-let top3List = [];
+
+
 let count = 0;
-console.log(sortScoreList);
-while (top3List.length <= 3) {
-  let curScore = sortScoreList.pop();
-  if (!top3List.includes(curScore)) {
-    top3List.push(curScore);
+let arr = [];
+
+while (arr.length < 3) {
+  let n = sortScoreList.pop();
+  if (!arr.includes(n)) {
+    arr.push(n);
   }
   count += 1;
-  console.log("top3List : " + top3List);
-  console.log("curScore : " + curScore);
-  console.log("count : " + count);
+  console.log('카운트 : ' + count);
+  console.log('값 : ' + n);
+  console.log('arr의 길이 : ' + arr.length);
+  console.log("-----------------------");
 }
+
 console.log(count);

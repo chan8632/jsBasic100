644acf42141af309df3d541470aea42154abca52
chan8632 <duscks8632@gmail.com>
2025-01-29 15:33:02 +0900

    문자열 압축하기(aab => a2b1로 바꾸는 작업)

--- 1.js
+++ 1.js
@@ -2,14 +2,18 @@
 const fs = require("fs");
 const input = fs.readFileSync("./tc.txt").toString().trim();
 
-let limit = (50 - input.length) / 2;
-let res = "";
-for (let i = 0; i < limit; i++) {
-  res += "=";
+const arr = input.split('');
+let obj = {};
+let res = '';
+for (let i = 0; i < arr.length; i++){
+  if (obj[arr[i]] === undefined) {
+    obj[arr[i]] = 1;
+  } else {
+    obj[arr[i]] += 1;
+  }
 }
-res += input;
-for (let j = 0; j < limit; j++){
-  res += "=";
+for (let idx in obj) {
+  res += idx;
+  res += obj[idx];
 }
-
-console.log(res.length);
+console.log(res);

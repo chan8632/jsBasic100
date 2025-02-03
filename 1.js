4b59adc462354aab02e7fb0ac8c9d56d3f6ee493
chan8632 <duscks8632@gmail.com>
2025-02-03 14:13:34 +0900

    forEach, map으로 두 개 배열 번갈아가면서 출력하는 문제 답안

--- 1.js
+++ 1.js
@@ -1,24 +1,35 @@
 "use strict";
 const fs = require("fs");
-// const input = fs.readFileSync("./tc.txt").toString().trim();
+const input = fs.readFileSync("./tc.txt").toString().trim();
 
-const input = 24;
-let N = parseInt(input, 10);
-let count = 0;
-while (N > 0) {
-  if (N >= 7) {
-    N -= 7;
-    count++;
-  } else if (N >= 3) {
-    N -= 3;
-    count++;
+const a = [1, 2, 3, 4];
+const b = ["a", "b", "c", "d"];
+let c = [];
+
+// a.forEach((val, idx) => {
+//   if (idx % 2 === 0) {
+//     c.push([val, b[idx]]);
+//   } else {
+//     c.push([b[idx], val]);
+//   }
+// })
+// console.log(c);
+
+let res = a.map((val, idx) => {
+  if (idx % 2 === 0) {
+    return [val, b[idx]];
   } else {
-    if (N === 0) {
-      break;
-    } else {
-      count = -1;
-      break;
-    }
+    return [b[idx], val];
   }
-}
-console.log(count);
+});
+
+console.log(res);
+// let res = [];
+// for (let i = 0; i < a.length; i++) {
+//   if (i % 2 === 0) {
+//     res.push([a[i], b[i]]);
+//   } else {
+//     res.push([b[i], a[i]]);
+//   }
+// }
+// console.log(res);

746506888ae21fbfaf72d01dcae79abb78db2c67
chan8632 <duscks8632@gmail.com>
2025-02-06 13:39:51 +0900

    [js 기초 100제] 버스 남은 시간 구하기 ( 답  풀이 )

--- 1.js
+++ 1.js
@@ -4,26 +4,27 @@ const input = fs.readFileSync("./tc.txt").toString().trim();
 
 const inputData = input.split("\n");
 const busScheduleList = JSON.parse(inputData[0]);
-const curTimeData = inputData[1].replace(/"/g, " ").split(":");
-const curHour = parseInt(curTimeData[0], 10);
-const curMinute = parseInt(curTimeData[1], 10);
-const curTime = curHour * 60 + curMinute;
-let scheduleHour = 0;
-let scheduleMinute = 0;
-let remainHour = '0';
+const curTimeData = inputData[1].replace(/"/g, " ").split(":").map((n)=>parseInt(n,10));
+// const curHour = parseInt(curTimeData[0], 10);
+// const curMinute = parseInt(curTimeData[1], 10);
+// const curTime = curHour * 60 + curMinute;
+// curTimeData[0]는 현재 시, curTimeData[1]는 현재 분
+const curTime = curTimeData[0] * 60 + curTimeData[1];
+let remainHour = 0;
 let remainMinute = 0;
-console.log(`curTime: ${curTime}`);
 let res = [];
 for (let busSchedule of busScheduleList) {
-  let busScheduleData = busSchedule.split(":");
-  scheduleHour = parseInt(busScheduleData[0], 10);
-  scheduleMinute = parseInt(busScheduleData[1], 10);
-  let scheduleTime = scheduleHour * 60 + scheduleMinute;
+  // 위와 같은 방법으로 처리
+  let busScheduleData = busSchedule.split(":").map((n) => parseInt(n, 10));
+  // scheduleHour = parseInt(busScheduleData[0], 10);
+  // scheduleMinute = parseInt(busScheduleData[1], 10);
+  // let scheduleTime = scheduleHour * 60 + scheduleMinute;
+  let scheduleTime = busScheduleData[0] * 60 + busScheduleData[1];
   let remainTime = scheduleTime - curTime;
   if (remainTime >= 0) {
     remainHour += parseInt(remainTime / 60, 10);
     remainMinute = parseInt(remainTime % 60, 10);
-    res.push(`${remainHour}시간 ${remainMinute}분`);
+    res.push(`${String(remainHour).padStart(2,0)}시간 ${String(remainMinute).padStart(2,0)}분`);
   } else {
     res.push("지나갔습니다.");
   }

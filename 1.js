6a20fd50f513ad3324e9c9bd61a127302e81abd1
chan8632 <duscks8632@gmail.com>
2025-02-12 14:35:33 +0900

    69번 에라토스의 체, sqrt, 스프레드연산자, indexOf, diff를 사용해서 정답 sqrt를 모든 수를 다 검사하는 걸 방지 스프레드 연산자와 indexOf를 이용해 차이가 가장 적게 나는 두 소수를 알아냄.

--- 1.js
+++ 1.js
@@ -2,31 +2,37 @@
 const fs = require("fs");
 const input = fs.readFileSync("./tc.txt").toString().trim();
 
-const inputData = input.split("\n");
-const busScheduleList = JSON.parse(inputData[0]);
-const curTimeData = inputData[1].replace(/"/g, " ").split(":").map((n)=>parseInt(n,10));
-// const curHour = parseInt(curTimeData[0], 10);
-// const curMinute = parseInt(curTimeData[1], 10);
-// const curTime = curHour * 60 + curMinute;
-// curTimeData[0]는 현재 시, curTimeData[1]는 현재 분
-const curTime = curTimeData[0] * 60 + curTimeData[1];
-let remainHour = 0;
-let remainMinute = 0;
+
+function prime_list(input) {
+  let n = parseInt(input, 10);
+  if (n < 2) return []; // 2 미만의 숫자는 소수 없음
+
+  let sieve = new Array(n + 1).fill(true);
+  sieve[0] = sieve[1] = false; // 0과 1은 소수가 아님
+
+  let m = Math.sqrt(n);
+  for (let i = 2; i <= m; i++) {
+    if (sieve[i]) {
+      //n은 n까지의 소수들을 구하는 과정
+      // j의 배수를 하나씩 없애가는 과정
+      // i * i부터 시작하는 이유는 i * i보다 작은 수들은 이미 다 소거가 돼서
+      for (let j = i * i; j <= n; j += i) {
+        sieve[j] = false;
+      }
+    }
+  }
+  return sieve
+    .map((value, idx) => (value ? idx : null))
+    .filter((value) => value !== null);
+}
+
+const inputNum = parseInt(input, 10);
+let arr = prime_list(inputNum);
 let res = [];
-for (let busSchedule of busScheduleList) {
-  // 위와 같은 방법으로 처리
-  let busScheduleData = busSchedule.split(":").map((n) => parseInt(n, 10));
-  // scheduleHour = parseInt(busScheduleData[0], 10);
-  // scheduleMinute = parseInt(busScheduleData[1], 10);
-  // let scheduleTime = scheduleHour * 60 + scheduleMinute;
-  let scheduleTime = busScheduleData[0] * 60 + busScheduleData[1];
-  let remainTime = scheduleTime - curTime;
-  if (remainTime >= 0) {
-    remainHour += parseInt(remainTime / 60, 10);
-    remainMinute = parseInt(remainTime % 60, 10);
-    res.push(`${String(remainHour).padStart(2,0)}시간 ${String(remainMinute).padStart(2,0)}분`);
-  } else {
-    res.push("지나갔습니다.");
+
+for (let prime of arr) {
+  if (arr.includes(inputNum - prime) && prime < inputNum - prime) {
+    res.push([prime, inputNum - prime]);
   }
 }
 

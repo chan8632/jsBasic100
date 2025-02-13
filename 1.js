f8b5604f1f1507d6775e1bc9e7e31251697d6fda
chan8632 <duscks8632@gmail.com>
2025-02-13 14:46:16 +0900

    [69번] 주어진 수를 두 소수의 합으로 표현 ( 정답풀이 및 설명주석 추가 )

--- 1.js
+++ 1.js
@@ -4,16 +4,19 @@ const input = fs.readFileSync("./tc.txt").toString().trim();
 
 const inputNum = parseInt(input, 10);
 function era(input) {
-  const trueList = Array(input + 1).fill(true);
-  //? sqrt는 어떻게 쓰지?
+  // 주어진 수가 2 미만이면 소수가 없음.
+  if (input < 2) return [];
+  // Array를 쓰려면 new를 써야 하나봄.
+  const trueList = new Array(input + 1).fill(true);
+  // 배열의 두 요소를 한꺼번에 할당
+  trueList[0] = trueList[1] = false;
+  
+  // sqrt를 쓰면 sqrt(input)이상의 합성수들은 알아서 걸러진다.
+  // 대신 밑에 for에서 범위를 input으로 해야 한다.
   const minArea = Math.sqrt(input);
-
-  trueList[0] = false;
-  trueList[1] = false;
-
-  for (let i = 2; i < trueList.length; i++) {
+  for (let i = 2; i < minArea; i++) {
     if (trueList[i] === true) {
-      for (let j = i * i; j < trueList.length; j += i) {
+      for (let j = i * i; j < input; j += i) {
         trueList[j] = false;
       }
     }
@@ -21,13 +24,13 @@ function era(input) {
   return trueList
     .map((bool, idx) => (bool === true ? idx : null))
     .filter((val) => val !== null);
-}
+} 
 
 let res = [];
 const primeList = era(inputNum);
 
 for (let prime of primeList) {
-  if (primeList.includes(inputNum - prime) && prime < inputNum - prime) {
+  if (primeList.includes(inputNum - prime) && (prime < inputNum - prime)) {
     res.push([prime, inputNum - prime]);
   }
 }
@@ -36,6 +39,6 @@ const diffPrimeList = res.map((prime) => prime[1] - prime[0]);
 const diffPrimeMinIdx = diffPrimeList.indexOf(Math.min(...diffPrimeList));
 const diffPrimeMaxIdx = diffPrimeList.indexOf(Math.max(...diffPrimeList));
 
-console.log("전체 : " + JSON.stringify(res));
-console.log("차가 가장 작은 두 수 : " + res[diffPrimeMinIdx]);
+console.log('전체 : ' + JSON.stringify(res));
+console.log('차가 가장 작은 두 수 : ' + res[diffPrimeMinIdx]);
 console.log("차가 가장 큰 두 수 : " + res[diffPrimeMaxIdx]);

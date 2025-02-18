70a4fcec4f84528d49250f4deb6d910566fd860d
chan8632 <duscks8632@gmail.com>
2025-02-18 16:17:04 +0900

    70번 배열 문제 (직사각형 배열도 고려) * 노션 정답지에는 정사각형 배열만 고려한 코드임

--- 1.js
+++ 1.js
@@ -3,31 +3,33 @@ const fs = require("fs");
 const input = fs.readFileSync("./tc.txt").toString().trim();
 
 function solution(a, b) {
-    const rowA = a.length;
-    const colA = a[0].length;
-    const rowB = b.length;
-    const colB = b[0].length;
+  const rowA = a.length;
+  const colA = a[0].length;
+  const rowB = b.length;
+  const colB = b[0].length;
 
-    if (colA !== rowB) {
-        return -1; // 행렬 곱셈 불가능
+  let c = Array.from({ length: rowA }, () => Array(colB).fill(0));
+
+  for (let i = 0; i < rowA; i++) {
+    for (let j = 0; j < colB; j++) {
+      for (let k = 0; k < colA; k++) {
+        c[i][j] += a[i][k] * b[k][j];
+      }
     }
+  }
 
-    let c = Array.from({ length: rowA }, () => Array(colB).fill(0));
-
-    for (let i = 0; i < rowA; i++) {
-        for (let j = 0; j < colB; j++) {
-            for (let k = 0; k < colA; k++) {
-                c[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
-
-    return c;
+  return c;
 }
 
-const a = [[1, 2, 3], [4, 5, 6]];
-const b = [[1, 4], [2, 5], [3, 6]];
+const a = [
+  [1, 2],
+  [2, 4],
+];
 
-console.log(solution(a, b)); 
+const b = [
+  [1, 0],
+  [0, 3],
+];
+
+console.log(solution(a, b));
 // 결과: [[14, 32], [32, 77]]
-

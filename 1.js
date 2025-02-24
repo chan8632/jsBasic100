2fec2ba109de7ac006848966dfda69cc4ad9e06b
chan8632 <duscks8632@gmail.com>
2025-02-24 18:33:12 +0900

    73번 최단경로 구하기 정답

--- 1.js
+++ 1.js
@@ -2,27 +2,33 @@
 const fs = require("fs");
 const input = fs.readFileSync("./tc.txt").toString().trim();
 const graph = {
-  A: ["E", "C", "B"],
-  B: ["A"],
-  C: ["A"],
-  D: ["E", "F"],
-  E: ["D", "A"],
-  F: ["D"],
+  A: ["B", "C"],
+  B: ["A", "D", "E"],
+  C: ["A", "F"],
+  D: ["B"],
+  E: ["B", "F"],
+  F: ["C", "E"],
 };
 
-// graph 자료구조와 startNode를 입력
-const BFS = (graph, startNode) => {
-  let visited = []; // 방문한 노드 기록 목록
-  let needVisit = []; // 방문해야 할 노드 목록(큐 역할, 방문대기목록)
-  needVisit.push(startNode); // 시작 노드를 방문 대기 목록에 추가
-  while (needVisit.length !== 0) { // 방문해야할 노드가 남아있다면 실행
-    let node = needVisit.shift(); // 큐에서 맨 앞 노드를 꺼냄
-    if (!visited.includes(node)) {
-      // 방문한 적이 없다면
-      visited.push(node); // 방문 기록 남기기
-      needVisit.push(...graph[node]); // 현재 노드는 큐에 추가 되어 있는 상태로 그 뒤에 현재 노드의 이웃 노드를 큐에 추가
+function shortDis(graph, start, end) {
+  if (start === end) return 0; // 시작 노드와 목표노드가 같다면 바로 0 리턴
+  let visited = new Set();
+  let queue = [[start, 0]];
+
+  while (queue.length > 0) { 
+    let [node, distance] = queue.shift(); // 큐에서 맨 앞 요소를 꺼냄
+    for (let neighbor of graph[node]) { // '최단 거리'가 중요하니 일단 인접노드부터 살펴보기
+      if (neighbor === end) {
+        return distance + 1; // 도착 노드 발견 시(제일 먼저 도착 노드를 검사), 현재 거리 + 1 반환
+      }
+      // 현재 노드의 인접 노드를 순회
+      if (!visited.has(node)) { // 중복 방지
+        visited.add(node); // 방문 기록
+        queue.push([neighbor, distance + 1]); // 인접 노드들을 큐에 추가하면서 거리 + 1
+      }
     }
   }
-  return visited; // 방문 순서 반환
-};
-console.log(BFS(graph, "E"));
+  return -1; // 경로가 없음
+}
+
+console.log(shortDis(graph, "F", "D"));

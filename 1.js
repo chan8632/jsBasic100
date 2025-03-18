ee95b7935b76fb4cdb1ade09028962493447dded
chan8632 <duscks8632@gmail.com>
2025-03-18 12:03:42 +0900

    78번 원형테이블 문제

--- 1.js
+++ 1.js
@@ -2,40 +2,32 @@
 const fs = require("fs");
 const input = fs.readFileSync("./tc.txt").toString().trim();
 
-const [inputOne, inputTwo] = input.split("\r\n");
-// THISISSTRINGS;
-// TATHISISKKQQAEW;
-
-// 어떻게 나눌 것인가?
-// 방법 1
-// 두 문자열을 하나씩 나누기
-// includes를 활용하나?
-
-// 동적 계획법을 이용해서 해결
-function longestCommonSubstring(str1, str2) {
-  // dp 테이블을 str1과 str2보다 하나 더 큰 크기도 만든다음 0으로 초기화한다.
-  // 그래야 일치하는 게 없을 때를 대비할 수 있다.
-  let dp = Array.from({ length: str1.length + 1 }, () => 
-    Array(str2.length + 1).fill(0)
-  );
-  let maxLength = 0;
-  // i와 j는 1부터 순회하는데 그래야 
-  for (let i = 1; i <= str1.length; i++){
-    for (let j = 1; j <= str2.length; j++){
-      if (str1[i - 1] === str2[j - 1]) {
-        // 같은 부분이 있을 때 대각선 왼쪽, 즉 자기보다 바로 전의 경우에서 + 1를 해준다.
-        // *** dp[i][j]는 str1의 i번째, str2의 j번째 문자열이 그 동안 얼마나 같은가를 말해주는 것.
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-        // 최대 크기 비교
-        maxLength = Math.max(dp[i][j], maxLength);
-      }
-      else {
-        // 연속해서 같은 문자가 없다면 0으로 초기화
-        dp[i][j] = 0;
-      }
+// countFood라는 걸 어떤 변수로 만들면 더 좋을까?
+const [allFood, countFood] = input.split(" ").map(Number);
+let circleTable = Array(allFood).fill(0).map((val, idx) => idx + 1);
+let eatIndex = 0;
+while (circleTable.length >= 3) {
+    if (eatIndex >= circleTable.length) {
+        // 배열을 초과한 경우, 실제로는 배열 길이만큼 앞으로 돌아와야 한다.
+        // 즉, 배열의 길이를 한 바퀴 돌았다고 가정하고, 초과한 만큼만 남긴다.
+        // 원래는 -로 구현했는데 그러면 숫자가 커질수록 여러번 빼야함
+        // 그러나 %로 구현하면 한 번에 뺄 수 있음
+        // % 는 -를 여러 번 구현한 것임.    
+        eatIndex %= circleTable.length;
     }
-  }
-  return maxLength;
+    circleTable.splice(eatIndex, 1);
+    // [2,3,4,5,6]
+    // [2,3,5,6]
+    // [3,5,6]
+    // [3,5]
+    console.log('남아있는 음식:' + circleTable);
+    // 자기자신을 빼고 순서를 세야하기에 -1를 해줘야 한다.
+    eatIndex += (countFood - 1);
+    // 2
+    // 4
+    // 0
+    // 2
+    console.log('없앨 음식 인덱스:' + eatIndex);
 }
 
-console.log(longestCommonSubstring(inputOne, inputTwo));
+console.log(circleTable);

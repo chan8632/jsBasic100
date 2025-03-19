d3390b3aff3bc285f11ac25a89fc2c1da246b2cd
chan8632 <duscks8632@gmail.com>
2025-03-19 12:06:42 +0900

    [79번] 순회하는 리스트 indexOf 사용

--- 1.js
+++ 1.js
@@ -2,32 +2,28 @@
 const fs = require("fs");
 const input = fs.readFileSync("./tc.txt").toString().trim();
 
-// countFood라는 걸 어떤 변수로 만들면 더 좋을까?
-const [allFood, countFood] = input.split(" ").map(Number);
-let circleTable = Array(allFood).fill(0).map((val, idx) => idx + 1);
-let eatIndex = 0;
-while (circleTable.length >= 3) {
-    if (eatIndex >= circleTable.length) {
-        // 배열을 초과한 경우, 실제로는 배열 길이만큼 앞으로 돌아와야 한다.
-        // 즉, 배열의 길이를 한 바퀴 돌았다고 가정하고, 초과한 만큼만 남긴다.
-        // 원래는 -로 구현했는데 그러면 숫자가 커질수록 여러번 빼야함
-        // 그러나 %로 구현하면 한 번에 뺄 수 있음
-        // % 는 -를 여러 번 구현한 것임.    
-        eatIndex %= circleTable.length;
+// 예를 들어 2번 순회했을 때 변하기 전의 리스트와 변한 후의 리스트의 값은 아래와 같습니다.
+
+// 순회전_리스트 = [10, 20, 25, 27, 34, 35, 39]
+// 순회후_리스트 = [35, 39, 10, 20, 25, 27, 34]
+
+// 2번
+// 왼 = 배열길이에서 - 2(입력한 수), 2(입력한 수) , 오 = 0(처음), 배열길이에서 - 2(입력한 수)
+
+const l = [10, 20, 25, 27, 34, 35, 39]; //기존 입력 값
+const n = parseInt(input, 10);
+let diffList = [];
+function rotate(inL, inN) {
+    let leftArr = [...inL].splice(inL.length - inN, inN);
+    let rightArr = [...inL].splice(0, inL.length - inN);
+    const rotArr = [...leftArr, ...rightArr];
+    for (let i = 0; i < inL.length; i++){
+        diffList.push(Math.abs(inL[i] - rotArr[i]));
     }
-    circleTable.splice(eatIndex, 1);
-    // [2,3,4,5,6]
-    // [2,3,5,6]
-    // [3,5,6]
-    // [3,5]
-    console.log('남아있는 음식:' + circleTable);
-    // 자기자신을 빼고 순서를 세야하기에 -1를 해줘야 한다.
-    eatIndex += (countFood - 1);
-    // 2
-    // 4
-    // 0
-    // 2
-    console.log('없앨 음식 인덱스:' + eatIndex);
+    const diffSortList = [...diffList].sort((a, b) => a - b);
+    const minIdx = diffList.indexOf(diffSortList[0]);
+    console.log('index: ' + minIdx);
+    console.log('value: ' + inL[minIdx] + ',' + rotArr[minIdx]);
 }
 
-console.log(circleTable);
+rotate(l, n);

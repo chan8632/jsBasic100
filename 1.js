6ff040d0af181dff9d096103d478d07b41934125
chan8632 <duscks8632@gmail.com>
2025-04-08 11:19:31 +0900

    89번 게임지도 그리기2 해설 보충

--- 1.js
+++ 1.js
@@ -2,89 +2,121 @@
 const fs = require("fs");
 const input = fs.readFileSync("./tc.txt").toString().trim().split("\r\n");
 
-const inputTable = input[0].split(" ").map(Number);
-const goalDish = parseInt(input[1], 10);
-function eatGoalDish(realTable, goalDishIdx) {
-  let sortTable = [...realTable].sort((a, b) => a - b);
-  goalDishIdx--;
-  let moveTableCount = 0;
-  while (true) {
-    // 우선, 현실 테이블에서 음식을 빼보고 그걸 가지고 판별
-    let currentDish = realTable.shift();
-    // 현 음식이 먹을 차례인 것
-    if (currentDish === sortTable[0]) {
-      // 먹을 음식이 목표 음식인 경우
-      if (goalDishIdx === 0) {
-        break;
-      }
-      // 먹을 음식이 목표 음식은 아닌 경우
-      else {
-        sortTable.shift();
-        goalDishIdx--;
-      }
+// 1. 맵을 2로 통일 ok
+// 2. 가로 세로 안쪽을 0으로 통일
+// 3. 캐릭터 위치를 1로 만들기
+// 4. 방애물 위치를 2로 통일
+const 가로 = 4;
+const 세로 = 5;
+const 캐릭터초기위치 = [0, 0];
+const 장애물 = [
+  [0, 1],
+  [1, 1],
+  [2, 3],
+  [1, 3],
+];
+console.log("이동 전 지도:");
+
+// const 캐릭터움직임원칙 = { 상: 1, 하: 2, 좌: 3, 우: 4 };
+const 움직임 = [2, 2, 2, 4, 4, 4];
+// 움직임에서 하나씩 이동경로 추출
+
+// 움직임에서 어디로 가는건지 숫자로 표현
+// 만약, 움직임이 추가한 위치가 2라면 continue
+
+function make_map(가로, 세로, 캐릭터위치, 장애물) {
+  let map = Array.from({ length: 세로 + 2 }, () => Array(가로 + 2).fill(2));
+  for (let i = 1; i <= 세로; i++) {
+    for (let j = 1; j <= 가로; j++) {
+      map[i][j] = 0;
     }
-    // 현 음식이 먹을 차례가 아닌 것
-    else {
-      // 안 먹기 때문에 일단 다시 음식을 회전테이블에 넣어두기
-      realTable.push(currentDish);
-      // 먹진 않으나 목표 음식은 맞는 경우
-      if (goalDishIdx === 0) {
-        goalDishIdx = realTable.length - 1;
-      } // 먹지도 않고 목표 음식도 아닌 경우
-      else {
-        goalDishIdx--;
-      }
-    }
-    console.log(realTable);
-    console.log('');
-    moveTableCount++;
   }
-  return moveTableCount;
+  const [캐릭터x좌표, 캐릭터y좌표] = [캐릭터위치[0] + 1, 캐릭터위치[1] + 1];
+  map[캐릭터x좌표][캐릭터y좌표] = 1;
+  for (let 장애물좌표 of 장애물) {
+    let 장애물x좌표 = 장애물좌표[0] + 1;
+    let 장애물y좌표 = 장애물좌표[1] + 1;
+    map[장애물x좌표][장애물y좌표] = 2;
+  }
+  [...map].forEach((row) => {
+    console.log(`[${row.join(", ")}]`);
+  });
+  return map;
 }
+const 지도 = make_map(가로, 세로, 캐릭터초기위치, 장애물);
+console.log("이동 후 지도");
 
-console.log(eatGoalDish(inputTable, goalDish));
+function move(지도, 움직임) {
+  let map = 지도;
+  let 캐릭터x좌표 = 0;
+  let 캐릭터y좌표 = 0;
+  for (let rowIdx = 0; rowIdx < map.length; rowIdx++) {
+    let row = map[rowIdx];
+    let colIdx = row.indexOf(1);
+    if (colIdx !== -1) {
+      캐릭터x좌표 = rowIdx;
+      캐릭터y좌표 = colIdx;
+    }
+  }
+  map[캐릭터y좌표][캐릭터x좌표] = 0;
+  for (let move of 움직임) {
+    if (move === 1 && map[캐릭터y좌표 - 1][캐릭터x좌표] !== 2) {
+      캐릭터y좌표--;
+    }
+    if (move === 2 && map[캐릭터y좌표 + 1][캐릭터x좌표] !== 2) {
+      캐릭터y좌표++;
+    }if (move === 3 && map[캐릭터y좌표][캐릭터x좌표 - 1] !== 2) {
+      캐릭터x좌표--;
+    }if (move === 4 && map[캐릭터y좌표][캐릭터x좌표 + 1] !== 2) {
+      캐릭터x좌표++;
+    }
+  }
+  map[캐릭터y좌표][캐릭터x좌표] = 1;
 
-// 5 2 3 1 2 5
-// 1
-
-// 목표는 1(첫번째 수)인 5점 접시를 먹는 것.
-// 흐름이
-// 목표인가? 먹는가? 2가지 경우를 설정해서 결국에 총 4가지의 케이스가 나옴
-// realTable이라는 현 회전초밥 상태를 나타내는 배열
-// sortTable이라는 숫자 순으로 정렬된 회전초밥을 나타내는 배열
-// goalDish라는 목표에 도달 했는지 필요한 인덱스 값
-// currentDish 라는 현 접시 인덱스
-
-// 예시 흐름
-// '5' 2 3 1 2 5
-// 목표 o 먹기 x
-
-// '2' 3 1 2 5 5
-// 목표 x 먹기 x
-
-// '3' 1 2 5 5 2
-// 목표 x 먹기 x
-
-// '1' 2 5 5 2 3
-// 목표 x 먹기 o
-
-// '2' 5 5 2 3
-// 목표 x 먹기 o
-
-// '5' 5 2 3
-// 목표 x 먹기 x
-
-// '5' 2 3 5
-// 목표 o 먹기 x
-
-// '2' 3 5 5
-// 목표 x 먹기 o
-
-// '3' 5 5
-// 먹기 o 목표 x
-
-// '5' 5
-// 먹기 o 목표 x
-
-// '5'
-// 먹기 o 목표 o
+  // for (let i = 0; i < 움직임.length; i++) {
+  //   let 이동경로 = 움직임[i];
+  //   if (이동경로 === 1) {
+  //     let 원본y좌표 = 캐릭터y좌표;
+  //     캐릭터y좌표--;
+  //     if (map[캐릭터y좌표][캐릭터x좌표] !== 2) {
+  //       map[캐릭터y좌표][캐릭터x좌표] = 1;
+  //       map[원본y좌표][캐릭터x좌표] = 0;
+  //     } else {
+  //       캐릭터y좌표 = 원본y좌표;
+  //     }
+  //   }
+  //   if (이동경로 === 2) {
+  //     let 원본y좌표 = 캐릭터y좌표;
+  //     캐릭터y좌표++;
+  //     if (map[캐릭터y좌표][캐릭터x좌표] !== 2) {
+  //       map[캐릭터y좌표][캐릭터x좌표] = 1;
+  //       map[원본y좌표][캐릭터x좌표] = 0;
+  //     } else {
+  //       캐릭터y좌표 = 원본y좌표;
+  //     }
+  //   }
+  //   if (이동경로 === 3) {
+  //     let 원본x좌표 = 캐릭터x좌표;
+  //     캐릭터x좌표--;
+  //     if (map[캐릭터y좌표][캐릭터x좌표] !== 2) {
+  //       map[캐릭터y좌표][캐릭터x좌표] = 1;
+  //       map[캐릭터y좌표][원본x좌표] = 0;
+  //     } else {
+  //       캐릭터x좌표 = 원본x좌표;
+  //     }
+  //   }
+  //   if (이동경로 === 4) {
+  //     let 원본x좌표 = 캐릭터x좌표;
+  //     캐릭터x좌표++;
+  //     if (map[캐릭터y좌표][캐릭터x좌표] !== 2) {
+  //       map[캐릭터y좌표][캐릭터x좌표] = 1;
+  //       map[캐릭터y좌표][원본x좌표] = 0;
+  //     } else {
+  //       캐릭터x좌표 = 원본x좌표;
+  //     }
+  //   }
+  // }
+  console.log(map);
+  console.log("캐릭터위치: ", [캐릭터x좌표, 캐릭터y좌표]);
+}
+const 캐릭터위치 = move(지도, 움직임);

8d05a66345c5fc8a5977bcdece11da3818c96daf
chan8632 <duscks8632@gmail.com>
2025-04-12 11:57:29 +0900

    91번 변수가 많다보니깐 헷갈린다

--- 1.js
+++ 1.js
@@ -7,54 +7,48 @@ const input = fs.readFileSync("./tc.txt").toString().trim();
 // 3. 반 1등 점수를 구하세요.
 // 4. 전교 평균을 구하세요.
 //한 반에 30명인 학생, 총 7개의 반
+//아래 코드는 힌트입니다.
+
 let student_score = [];
 let class_score = [];
 let total_score = [];
 
-function generate_student_score() {
-  student_score = [];
-  for (let i = 0; i < 5; i++) {
-    student_score.push(Math.floor(Math.random() * 100) + 1);
-  }
-  return student_score;
-}
-
-function generate_class_score() {
+for (let i = 0; i < 7; i++) {
   class_score = [];
-  for (let i = 0; i < 30; i++) {
-    class_score.push(generate_student_score());
-  }
-  return class_score;
-}
-
-function generate_total_score() {
-  total_score = [];
-  for (let i = 0; i < 7; i++) {
-    total_score.push(generate_class_score());
-  }
-  return total_score;
-}
-
-class ClassInfo {
-  constructor() {
-    this.school = generate_class_score();
-  }
-  findClassAvg() {
-    let avg_score = this.school;
-    for (let i = 0; i < 7; i++) {
-      let classTotal = 0;
-      for (let j = 0; j < 30; j++) {
-        let studentTotal = 0;
-        for (let k = 0; k < 5; k++) {
-          studentTotal += avg_score[i][j][k];
-        }
-        avg_score[i][j] = studentTotal;
-        classTotal += avg_score[i][j];
-      }
-      avg_score[i] = Math.floor(classTotal / 150);
+  for (let j = 0; j < 30; j++) {
+    student_score = [];
+    for (let k = 0; k < 5; k++) {
+      student_score.push(Math.floor(Math.random() * 100) + 1);
     }
-    return avg_score;
-  }  
+    class_score.push(student_score);
+  }
+  total_score.push(class_score);
 }
-const school = new ClassInfo();
-console.log(school.findClassAvg());
+
+
+let student_sum = 0;
+let studentAvg = 0;
+let classSum = 0;
+let classAvg = 0;
+let totalAverage = [];
+
+for (let c of total_score){
+  let classTotal = [];
+  for (let student of c) {
+    student_sum = student.reduce((a, b) => a + b);
+    studentAvg = student_sum / 5;
+    classTotal.push(studentAvg);
+  }
+  console.log(`${total_score.indexOf(c) + 1}반 평균 : `);
+  console.log(classTotal);
+  console.log(`${total_score.indexOf(c) + 1}반 일등 : `);
+  console.log(Math.max(...classTotal));
+  console.log('=======================================');
+
+  classSum = classTotal.reduce((a, b) => a + b);
+  classAvg = classSum / 30;
+  totalAverage.push(classAvg);
+}
+console.log('전체 평균:')
+console.log(totalAverage);
+// console.log(total_score);

8ebf377c89885628bed1736b11313ab82fd81ddc
chan8632 <duscks8632@gmail.com>
2025-04-14 12:03:43 +0900

    91번 전교생 점수 구하기 다시 풀기

--- 1.js
+++ 1.js
@@ -12,43 +12,50 @@ const input = fs.readFileSync("./tc.txt").toString().trim();
 let student_score = [];
 let class_score = [];
 let total_score = [];
-
 for (let i = 0; i < 7; i++) {
   class_score = [];
   for (let j = 0; j < 30; j++) {
     student_score = [];
     for (let k = 0; k < 5; k++) {
-      student_score.push(Math.floor(Math.random() * 100) + 1);
+      student_score.push(Math.floor(Math.random() * 101));
     }
     class_score.push(student_score);
   }
   total_score.push(class_score);
 }
 
-
-let student_sum = 0;
-let studentAvg = 0;
-let classSum = 0;
-let classAvg = 0;
-let totalAverage = [];
-
-for (let c of total_score){
-  let classTotal = [];
-  for (let student of c) {
-    student_sum = student.reduce((a, b) => a + b);
-    studentAvg = student_sum / 5;
-    classTotal.push(studentAvg);
-  }
-  console.log(`${total_score.indexOf(c) + 1}반 평균 : `);
-  console.log(classTotal);
-  console.log(`${total_score.indexOf(c) + 1}반 일등 : `);
-  console.log(Math.max(...classTotal));
-  console.log('=======================================');
-
-  classSum = classTotal.reduce((a, b) => a + b);
-  classAvg = classSum / 30;
-  totalAverage.push(classAvg);
-}
-console.log('전체 평균:')
-console.log(totalAverage);
+// console.log("반 점수 모두가 담긴 전교 점수 다중 리스트");
 // console.log(total_score);
+
+// 2. 반 평균을 구하세요. ok
+
+// 3. 반 1등 점수를 구하세요. ok
+
+// 4. 전교 평균을 구하세요.
+
+let studentSum = 0;
+let classSum = 0;
+let classAvgList = [];
+let classFirstList = [];
+let totalAvg = [];
+for (let eathClass of total_score) {
+  let eachClassEachScoreSumList = [];
+  for (let eathStudent of eathClass) {
+    studentSum = eathStudent.reduce((a, b) => a + b);
+    eachClassEachScoreSumList.push(studentSum);
+  }
+  let eachClassSum = eachClassEachScoreSumList.reduce((a, b) => a + b);
+  let eachClassAvg = Math.floor(eachClassSum / 150);
+  let eachClassFirst = Math.floor(Math.max(...eachClassEachScoreSumList) / 5);
+  classAvgList.push(eachClassAvg);
+  classFirstList.push(eachClassFirst);
+  totalAvg.push(eachClassSum);
+}
+console.log("반 평균");
+console.log(classAvgList);
+
+console.log("반 일등들");
+console.log(classFirstList);
+
+console.log("전교 평균");
+console.log(Math.floor(totalAvg.reduce((a,b)=> a + b) / (5* 30 * 7)));
